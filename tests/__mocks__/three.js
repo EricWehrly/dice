8514330b8be1d3e78462cdac2d7bc1d10243d405
EricWehrly <eric.wehrly@gmail.com>
2025-12-19 00:36:15 -0800

    fuller mock

--- tests/__mocks__/three.js
+++ tests/__mocks__/three.js
@@ -11,7 +11,22 @@ const THREE = {
         setSize: jest.fn(),
         render: jest.fn(),
     })),
-    Mesh: jest.fn().mockImplementation(() => new Mesh()),
+    Mesh: jest.fn().mockImplementation(() => ({
+        geometry: {},
+        children: [],
+        position: { x: 0, y: 0, z: 0, set: jest.fn() },
+        rotation: { x: 0, y: 0, z: 0 },
+        add: jest.fn(),
+    })),
+    BoxGeometry: jest.fn().mockImplementation(() => ({})),
+    TetrahedronGeometry: jest.fn().mockImplementation(() => ({})),
+    OctahedronGeometry: jest.fn().mockImplementation(() => ({})),
+    DodecahedronGeometry: jest.fn().mockImplementation(() => ({})),
+    IcosahedronGeometry: jest.fn().mockImplementation(() => ({})),
+    MeshStandardMaterial: jest.fn().mockImplementation(() => ({})),
+    MeshBasicMaterial: jest.fn().mockImplementation(() => ({})),
+    SphereGeometry: jest.fn().mockImplementation(() => ({})),
+    Group: jest.fn().mockImplementation(() => ({ add: jest.fn(), children: [], position: { set: jest.fn() } })),
     // Add other necessary mocks here
 };
 
